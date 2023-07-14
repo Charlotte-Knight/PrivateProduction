08ea22db50270dc8098d0a2f4e311b882b06cd3c
Matthew Knight <mdk16@ic.ac.uk>
2023-07-14 17:03:39 +0100

    Keep track of input and output files

--- parse_setups.py
+++ parse_setups.py
@@ -3,12 +3,27 @@ import os
 
 REDOWNLOAD = False
 
-def processCmsDriver(command):
+def processCmsDriver(command, filein=None, fileout=None):
   command = command.replace(" || exit $? ;", "")
   command = command.replace("--no_exec", "")
 
+  if filein is not None:
+    assert "--filein" in command
+    filename = command.split("--filein ")[1].split(" ")[0]
+    filein_part = "--filein "+filename
+
+    command = command.replace(filein_part, " ")
+    command += f" --filein {filein}"
+
+  if fileout is not None:
+    assert "--fileout" in command
+    filename = command.split("--fileout ")[1].split(" ")[0]
+    fileout_part = "--fileout "+filename
+
+    command = command.replace(fileout_part, " ")
+    command += f" --fileout {fileout}"
+
   if "GEN,LHE" in command:
-    #command = command.replace("Configuration", "${CMSSW_BASE}/src/Configuration")
     command += ' --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})"'
   return command
 
@@ -27,7 +42,7 @@ for era in setup_urls.keys():
 
   config[era] = []
 
-  for url in setup_urls[era]:
+  for stage_i, url in enumerate(setup_urls[era]):
     name = url.split("/")[-1]
     if (not os.path.exists(name)) or REDOWNLOAD:
       os.system(f"wget -O {name} {url} --no-check-certificate")
@@ -36,19 +51,27 @@ for era in setup_urls.keys():
       setup = f.read()
 
     stage_config = {}
+    stage_config["extra_commands"] = setup.split("eval `scram runtime -sh`")[1].split("scram b")[0]
+    stage_config["stage_name"] = name
+
+    if stage_i != 0:
+      filein = "file:"+config[era][-1]["stage_name"]+".root"
+    else:
+      filein = None
+    fileout = "file:"+name+".root"
+
     for line in setup.split("\n"):
-      if "cmsDriver" in line:
-        stage_config["cmsDriver"] = processCmsDriver(line)
+      if "cmsDriver.py" in line:
+        stage_config["cmsDriver"] = processCmsDriver(line, filein, fileout)
       elif "scram p" in line:
         stage_config["CMSSW_version"] = "CMSSW" + line.split("CMSSW")[-1]
 
-      stage_config["extra_commands"] = setup.split("eval `scram runtime -sh`")[1].split("scram b")[0]
-      stage_config["stage_name"] = name
+      
 
     config[era].append(stage_config)
 
 os.chdir(config_dir)
 with open("config.json", "w") as f:
-  json.dump(config, f)
+  json.dump(config, f, indent=2)
 
   
